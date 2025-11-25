f4fdbff083400ec3f47ed8b638c0f0659a3e96c0
tangkikodo <allmonday@126.com>
2025-11-25 22:22:44 +0800

    bump pydantic-resolve version

--- src/router/demo/schema.py
+++ src/router/demo/schema.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from pydantic_resolve import Loader, Collector
+from typing import Optional, Annotated
+from pydantic_resolve import Loader, LoadBy
 from src.services.story.schema import Story as BaseStory
 
 from src.services.task.schema import Task as BaseTask
@@ -8,17 +8,9 @@ from src.services.task.loader import story_to_task_loader
 from src.services.user.schema import User as BaseUser
 from src.services.user.loader import user_batch_loader
 
-
 class Task(BaseTask):
-    user: Optional[BaseUser] = None
-    def resolve_user(self, loader=Loader(user_batch_loader)):
-        return loader.load(self.owner_id) if self.owner_id else None
+    user: Annotated[Optional[BaseUser], LoadBy('owner_id')] = None
 
 class Story(BaseStory):
-    tasks: list[Task] = []
-    def resolve_tasks(self, loader=Loader(story_to_task_loader)):
-        return loader.load(self.id)
-
-    assignee: Optional[BaseUser] = None
-    def resolve_assignee(self, loader=Loader(user_batch_loader)):
-        return loader.load(self.owner_id) if self.owner_id else None
+    tasks: Annotated[list[Task], LoadBy('id')] = []
+    assignee: Annotated[Optional[BaseUser], LoadBy('owner_id')] = None
